d213ded6e0e226a5a5fdaac779aa29c6a7816842
jasZnerol <jasper@lorenzmail.de>
2019-10-25 14:41:58 +0200

    Project transfer from our own repository (pre lecture) 2/3

---
+++ src/app/gui/GuiController.java
@@ -0,0 +1,306 @@
+package app.gui;
+
+//Imports for the GUI
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.fxml.FXML;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import app.game.City;
+import app.game.E_Outbreak;
+import app.game.Event;
+import app.game.EventType;
+import app.game.Game;
+import app.game.Virus;
+import app.http.GameExchange;
+import app.http.GameServer;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+public class GuiController {
+	
+	//All Buttons (each button corresponds to one event.
+	@FXML
+	private Button quitB, endRoundB, putUnderQuarantineB, closeAirportB, closeConnectionB,
+	developVaccineB, deployVaccineB, developMedicationB, deployMedicationB, getGameB,
+	applyHygienicMeasuresB, exertInfluenceB, callElectionsB, launchCampaignB, medicateBiggestCitiesB;
+	
+	private GameExchange currentGame;
+	private MapController mapControllerRefrence;
+	
+	@FXML
+	private TextField cityfrom, cityto, rounds, pathogen, amountT;
+	
+	@FXML
+	private Text currentRound, currentPoints;
+	
+	
+	//Constructor
+	public GuiController () {
+		/*
+		 * Has no real purpose. 
+		 * The Class-Objects (GUI-elements) will be assigned
+		 * to the corresponding element in the fxml-file.
+		 */
+		System.out.println("GuiController has been created.");
+	}
+	
+	@FXML
+	public void initialize () {
+		
+		System.out.println("GuiController has been initialized.");
+		
+		//Setting up a prompt text in the textField. Therefore when selected it will disappear.
+		this.cityfrom.setPromptText("enter city...");
+		this.cityto.setPromptText("enter city...");
+		this.rounds.setPromptText("enter duration...");
+		this.pathogen.setPromptText("enter pathogen...");
+		this.amountT.setPromptText("enter the amount ...");
+	
+		//Set Points and current Round.
+		if(this.currentGame != null) {
+			this.currentRound.setText(this.currentGame.getGame().getRound() + "");
+//			MapFactory.createMap(this.currentGame.getGame());			
+			
+			//uncomment when points are implemented.
+			this.currentPoints.setText(this.currentGame.getGame().getPoints() + "");
+			
+		}
+	}	
+	
+	public void setMapResource (MapController mapController) {
+		
+		this.mapControllerRefrence = mapController;
+	}
+	
+	//Button-Events:
+	@FXML
+	private void quit () {
+		
+//		System.out.println("quit");
+		this.mapControllerRefrence.quit();
+		Stage primaryStage = (Stage) quitB.getScene().getWindow();
+		if (primaryStage == null) System.out.println("error");
+        primaryStage.close();
+	}
+	
+	@FXML
+	private void endRound () {
+		
+		String cityAmount = this.amountT.getText();
+		int amount = 1;
+		if(cityAmount != null) {
+			try {
+				amount = Integer.parseInt(cityAmount);
+			} catch (NumberFormatException e) {
+//				Amount field empty
+			}
+		}
+		
+		for (int i = 0; i < amount; i++) {
+			
+			GameServer.addReply((Game g)-> "{\"type\": \"endRound\"}");
+		}
+		
+		System.out.println("endRound");
+		this.executeEvent(GameServer.getReply().evalutate(currentGame.getGame()));
+		
+	}
+	
+	@FXML
+	private void putUnderQuarantine () {
+		
+		System.out.println("quarantine");
+		this.executeEvent("{\"type\": \"putUnderQuarantine\", \"city\":\"" + this.cityfrom.getText() +
+						"\", \"rounds\": " + this.rounds.getText() + "}");
+	}
+	
+	@FXML
+	private void closeAirport () {
+	
+		System.out.println("closeAirport");
+		this.executeEvent("{\"type\": \"closeAirport\", \"city\": \"" + this.cityfrom.getText() +
+						"\", \"rounds\": " + this.rounds.getText() + "}");
+	}	
+	
+	@FXML
+	private void closeConnection () {
+		
+		System.out.println("closeConnection");
+		this.executeEvent("{\"type\": \"closeConnection\", \"fromCity\": \"" + this.cityfrom.getText() +
+						"\", \"toCity\": \"" + this.cityto.getText() +  "\", \"rounds\": " + 
+						this.rounds.getText() + "}");
+	}
+	
+	@FXML 
+	private void developVaccine () {
+		
+		System.out.println("developVaccine");
+		this.executeEvent("{\"type\": \"developVaccine\", \"pathogen\": \"" +
+						this.pathogen.getText() + "\"}");
+	}
+	
+	@FXML
+	private void deployVaccine () {
+		
+		String cityAmount = this.amountT.getText();
+		String selectedPathogen = this.pathogen.getText();
+
+		if(selectedPathogen.equals("")) return;
+		
+		if (this.cityfrom.getText().equals("")) {
+			
+			//check if the actions can be added.
+			if (cityAmount == null || selectedPathogen == null) return;
+			
+			//convert to Integer
+			int cityAmountInt = 0;
+			try {
+				cityAmountInt = Integer.parseInt(cityAmount);
+			} catch (NumberFormatException e) {
+//				Amount field empty
+			}
+			
+			for (int i = 0; i < cityAmountInt; i++) {
+				
+				GameServer.addReply((Game g)-> {
+				Collection<City> cities = g.getCities().values();
+				ArrayList<Pair<City, Double>> citiesPrev = new ArrayList<>();
+				
+				//get all cities that are infected by the selected Pathogen.
+				for (City c : cities) {
+					double prev = 0;
+					boolean alreadyVacc = false;
+					for(Event e : g.getFromCityEventMap(c)) {
+						if (e.getType() == EventType.outbreak) {
+							if (selectedPathogen.equals(((E_Outbreak) e).getVirus().getName()))
+								prev = ((E_Outbreak) e).getPrevalence();
+						} else if(e.getType() == EventType.vaccineDeployed) {
+							alreadyVacc = true;
+						}
+					}
+					if(!alreadyVacc)
+						citiesPrev.add(new Pair<>(c, prev));
+				}
+					
+					//sort the infectedCities by size.
+					//don't ask questions. It works.
+					citiesPrev.sort((Pair<City, Double> c2, Pair<City, Double> c1)->
+								(int) ((c1.getKey().getCitizens() * (1 - c1.getValue()))
+								- (c2.getKey().getCitizens()) * (1 - c2.getValue())));
+					
+					return "{\"type\": \"deployVaccine\", \"pathogen\": \"" + selectedPathogen +
+							"\", \"city\": \"" + citiesPrev.get(0).getKey().getName() + "\"}";
+				});
+				
+			}
+			
+			this.executeEvent(GameServer.getReply().evalutate(currentGame.getGame()));
+		} else {
+			
+			System.out.println("developVaccine");
+			this.executeEvent("{\"type\": \"deployVaccine\", \"pathogen\": \"" + selectedPathogen +
+					"\", \"city\": \"" + this.cityfrom.getText() + "\"}");
+		}
+	}
+	
+	@FXML
+	private void developMedication () {
+		
+		System.out.println("developMedication");
+		this.executeEvent("{\"type\": \"developMedication\", \"pathogen\": \""
+						+ this.pathogen.getText() + "\"}");
+	}
+	
+	@FXML
+	private void deployMedication () {
+		
+		System.out.println("deployMedication");
+		this.executeEvent("{\"type\": \"deployMedication\", \"pathogen\": \"" + this.pathogen.getText() +
+							"\", \"city\": \"" + this.cityfrom.getText() + "\"}");
+	}
+	
+	@FXML
+	private void applyHygienicMeasures () {
+		
+		System.out.println("applyHygienicMeasures");
+		this.executeEvent("{\"type\": \"applyHygienicMeasures\", \"city\":\"" + this.cityfrom.getText() + "\"}");
+	}
+	
+	@FXML 
+	private void exertInfluence () {
+		
+		System.out.println("exertInfluence");
+		this.executeEvent("{\"type\": \"exertInfluence\", \"city\": \"" + this.cityfrom.getText() + "\"}");
+	}
+	
+	@FXML 
+	private void callElections () {
+		
+		System.out.println("callElections");
+		this.executeEvent("{\"type\": \"callElections\", \"city\": \"" + this.cityfrom.getText() + "\"}");
+	}
+	
+	@FXML
+	private void launchCampaign () {
+		
+		System.out.println("launchCampaign");
+		this.executeEvent("{\"type\": \"launchCampaign\", \"city\": \"" + this.cityfrom.getText() + "\"}");
+	}
+	
+	@FXML //Button implementation
+	private void medicateBiggestCities () {
+		
+		String cityAmount = this.amountT.getText();
+		String selectedPathogen = this.pathogen.getText();
+		
+		//check if the actions can be added.
+		if (cityAmount == null || selectedPathogen == null) return;
+		
+		//convert to Integer
+		int cityAmountInt = Integer.parseInt(cityAmount);
+		
+		for (int i = 0; i < cityAmountInt; i++) {
+			
+			GameServer.addReply((Game g)-> {
+				HashSet<E_Outbreak> outbreaks = g.getOutbreakEvents();
+				ArrayList<Pair<City, Double>> infectedCities = new ArrayList<>();
+				
+				//get all cities that are infected by the selected Pathogen.
+				for (E_Outbreak e : outbreaks) {
+					if (e.getVirus().getName().equals(selectedPathogen)) 
+						infectedCities.add(new Pair<City, Double>(e.getCity(), e.getPrevalence()));
+				}
+				
+				//sort the infectedCities by size.
+				//don't ask questions. It works.
+				infectedCities.sort((Pair<City, Double> c2, Pair<City, Double> c1)->
+							(int) ((c1.getKey().getCitizens() * c1.getValue())
+							- (c2.getKey().getCitizens()) * c2.getValue()));
+				
+				return "{\"type\": \"deployMedication\", \"pathogen\": \"" + selectedPathogen +
+				"\", \"city\": \"" + infectedCities.get(0).getKey().getName() + "\"}";
+			});
+			
+		}
+		
+		this.executeEvent(GameServer.getReply().evalutate(currentGame.getGame()));
+	}
+	
+	private void executeEvent(String event) {
+		if(this.currentGame == null || !this.currentGame.isAlive()) return;
+		this.currentGame.sendReply(event);
+		
+		this.quit();
+		this.mapControllerRefrence.quit();
+	}
+	
+	public void setGame(GameExchange exchange) {
+		this.currentGame = exchange;
+	}
+}
---
+++ src/app/io/MapFactory.java
@@ -0,0 +1,77 @@
+package app.io;
+
+import java.awt.Color;
+
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import app.game.City;
+import app.game.E_Outbreak;
+import app.game.Game;
+
+public class MapFactory {
+	public static void createMap(Game game) {
+		try {
+			// Dimensions
+			int width = 3600, height = 1800;
+ 
+			// Create image
+			BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+  
+			// Get canvas
+			Graphics2D g2d = bi.createGraphics();
+ 
+			// Fill background
+			g2d.setColor(Color.WHITE);
+			g2d.fillRect(0, 0, width, height);
+  
+			// Prettier lines
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+			RenderingHints.VALUE_ANTIALIAS_ON);
+ 
+			// Font for city names
+			Font font = new Font("TimesRoman", Font.BOLD, 20);
+			g2d.setFont(font);
+  
+			for(City c : game.getCities().values()) {
+				// Draw city circle
+				// Coordinates
+				int prev = 0;
+				for(E_Outbreak e: game.getOutbreakEvents()) {
+					if(e.getCity() == c) prev = (int) (255 * e.getPrevalence());
+				}
+				Color red = new Color(prev, 0, 0, 100);
+				g2d.setColor(red);
+				int x = (int) c.getX() + 180, y = (int) - c.getY() + 90;
+				int diameter = c.getCitizens() / 100;
+				x *= 10;
+				y *= 10;
+				// Inner
+				Ellipse2D.Double circle = new Ellipse2D.Double(x - diameter / 2, y - diameter / 2, diameter, diameter);
+				g2d.fill(circle);
+				//Outer
+				g2d.setColor(Color.BLACK);
+				g2d.drawOval(x - diameter / 2, y - diameter / 2, diameter, diameter);
+				
+				//Draw name
+				FontMetrics fontMetrics = g2d.getFontMetrics();
+		    	int stringWidth = fontMetrics.stringWidth(c.getName());
+		    	int stringHeight = fontMetrics.getAscent();
+				g2d.drawString(c.getName(), x - stringWidth / 2, y + stringHeight / 4);
+			}
+
+			ImageIO.write(bi, "PNG", new File("Map" + game.getRound() +".PNG"));
+	      
+		} catch (IOException ie) {
+	    	ie.printStackTrace();
+	    }
+	}
+}
