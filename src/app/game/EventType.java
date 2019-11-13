3f1f441b220b3511d8e482be02b47dc99ff19c4e
ChristophScn <christoph@hoebbel-schnell.de>
2019-11-13 11:57:01 +0100

    Added function for EventType

--- src/app/game/EventType.java
+++ src/app/game/EventType.java
@@ -2,8 +2,19 @@ package app.game;
 
 public enum EventType {
 
-	airportClosed,antiVaccinationism,bioTerrorism,connectionClosed,medicationAvailable,
-	medicationDeployed,medicationInDevelopment,outbreak,pathogenEncountered,quarantine,
-	uprising,vaccineAvailable,vaccineDeployed,vaccineInDevelopment;
-	
+	airportClosed, antiVaccinationism, bioTerrorism, connectionClosed, medicationAvailable, medicationDeployed,
+	medicationInDevelopment, outbreak, pathogenEncountered, quarantine, uprising, vaccineAvailable, vaccineDeployed,
+	vaccineInDevelopment;
+
+	public boolean containedByCity() {
+		return  this == airportClosed		||
+				this == antiVaccinationism	||
+				this == bioTerrorism		||
+				this == connectionClosed	||
+				this == medicationDeployed	||
+				this == outbreak			||
+				this == quarantine			||
+				this == uprising			||
+				this == vaccineDeployed;
+	}
 }
