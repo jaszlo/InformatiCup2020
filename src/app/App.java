f6c97dcf78bae76428514d84a2848741a53f6584
jasper@lorenzmail.de <jasper@lorenzmail.de>
2019-12-20 13:50:35 +0100

    renamed Testing.java to App.java

--- src/app/App.java
+++ src/app/App.java
@@ -11,7 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Testing extends Application {
+public class App extends Application {
 	
 	public static GuiController guiController;
 			
