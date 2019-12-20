1665e8153aefa841054da3d3c285774ae2f11279
jasper@lorenzmail.de <jasper@lorenzmail.de>
2019-12-20 01:27:52 +0100

    Modified winrate print

--- TestDeployment.py
+++ TestDeployment.py
@@ -7,15 +7,15 @@ loss = 0
 def playGame(seed):
     global wins
     global loss
-    result = subprocess.run(['./ic20_linux', '-t', '0', '-s', str(seed), '-u', 'https://udi8pt9vo9.execute-api.us-east-1.amazonaws.com/Beta/'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    result = subprocess.run(['./ic20_linux', '-t', '0', '-s', str(seed),"""'-u', 'https://udi8pt9vo9.execute-api.us-east-1.amazonaws.com/Beta/'"""], stdout=subprocess.PIPE).stdout.decode('utf-8')
     if("win" in result):
-        wins += 1
-        print("Seed: %d Outcome: Win" % (seed))
+        wins += 1   
+        print("Game: %d    Seed: %d    Outcome: Win" % ((wins + loss), seed))
         print("Winrate %f%%" % (wins * 100 / (wins + loss)))
         return (seed, "win")
     elif("loss" in result):
-        loss += 1
-        print("Seed: %d Outcome: Loss" % (seed))
+        loss += 1 
+        print("Game: %d    Seed: %d    Outcome: Win" % ((wins + loss), seed))
         print("Winrate %f%%" % (wins * 100 / (wins + loss)))
         return (seed, "loss")
 
