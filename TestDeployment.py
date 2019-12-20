54ff6837b4505acfcea5ab01f5212fffa63e12d4
jasper@lorenzmail.de <jasper@lorenzmail.de>
2019-12-20 01:35:07 +0100

    removed comment around url flag

--- TestDeployment.py
+++ TestDeployment.py
@@ -7,7 +7,7 @@ loss = 0
 def playGame(seed):
     global wins
     global loss
-    result = subprocess.run(['./ic20_linux', '-t', '0', '-s', str(seed),"""'-u', 'https://udi8pt9vo9.execute-api.us-east-1.amazonaws.com/Beta/'"""], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    result = subprocess.run(['./ic20_linux', '-t', '0', '-s', str(seed), '-u', 'https://udi8pt9vo9.execute-api.us-east-1.amazonaws.com/Beta/'], stdout=subprocess.PIPE).stdout.decode('utf-8')
     if("win" in result):
         wins += 1   
         print("Game: %d    Seed: %d    Outcome: Win" % ((wins + loss), seed))
@@ -24,10 +24,12 @@ pool = ThreadPool(10)
 
 # Open the URLs in their own threads
 # and return the results
-results = pool.map(playGame, list(range(1, 101)))
+results = pool.map(playGame, list(range(1, 1001)))
 
 print(results)
 
 # Close the pool and wait for the work to finish
 pool.close()
 pool.join()
+
+
