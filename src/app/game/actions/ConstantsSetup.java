74e108a44886e075b68ec102a4943a22123e87c8
Robin <Robinyyy@t-online.de>
2019-12-24 15:30:49 +0100

    added documentation to new event classes

--- src/app/game/actions/ConstantsSetup.java
+++ src/app/game/actions/ConstantsSetup.java
@@ -10,7 +10,14 @@ public class ConstantsSetup {
 	
 	public static final String CONSTANTS_PATH = "resources/constants.txt";
 	
-	
+	/**
+	 * Maps values of constants from a specified file.
+	 * Reads from a specified file. If the file does not exist or can't be opened
+	 * null is returned. Otherwise each line of the format %string %double results
+	 * in an entry in the resulting Map. Empty lines or lines that start with '//' are ignored.
+	 * @param resourceFilename the path to file of constants relative to resource folder.
+	 * @return a map of constants that were parsed from the file
+	 */
 	public static HashMap<String,Double> getConstants(String resourceFilename){
 		HashMap<String,Double> constants = new HashMap<String,Double>();
 		File constFile = FileHandler.getFileFromResources(resourceFilename);
--- src/app/game/events/E_InfluenceExerted.java
+++ src/app/game/events/E_InfluenceExerted.java
@@ -2,22 +2,38 @@ package app.game.events;
 
 import app.game.City;
 
+/**
+ *  Class to hold information about the ingame InfluenceExerted Event
+ */
 public class E_InfluenceExerted extends Event{
 
 	private final City city;
 	
 	private final int round;
 	
+	/**
+	 * 
+	 * @param round the round in which this event occoured
+	 * @param city the city in which the event took place
+	 */
 	public E_InfluenceExerted(City city, int round) {
 		super(EventType.influenceExerted);
 		this.city = city;
 		this.round = round;
 	}
 
+	/**
+	 * 
+	 * @return city the city in which the event took place
+	 */
 	public City getCity() {
 		return city;
 	}
 
+	/**
+	 * 
+	 * @return round the round in which this event occoured
+	 */
 	public int getRound() {
 		return round;
 	}
